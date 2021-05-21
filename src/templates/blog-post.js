7ece60f1f36ce66c05881192d22b545859b8462d
Simon Achioya <tomiwaachioya@gmail.com>
2021-05-21 12:44:47 +0100

    done

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -24,7 +24,7 @@ const BlogPostTemplate = ({ data, location }) => {
         itemType="http://schema.org/Article"
       >
         <header>
-          <h1 itemProp="headline" className={styles.frontmatter.title}>
+          <h1 itemProp="headline" className={styles.title}>
           {post.frontmatter.title}</h1>
           <p>{post.frontmatter.date}</p>
         </header>
